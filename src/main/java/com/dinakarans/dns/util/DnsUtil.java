96358a65041c03c4510a3046e99fab165dca1af6
Dinakaran S <87858726+Duke98666@users.noreply.github.com>
2024-09-24 00:59:09 +0530

    Forwarding Server

--- src/main/java/com/dinakarans/dns/util/DnsUtil.java
+++ src/main/java/com/dinakarans/dns/util/DnsUtil.java
@@ -112,6 +112,7 @@ public final class DnsUtil {
             byteBuffer.get(rDataBytes);
             answer.setRData(Arrays.copyOf(rDataBytes, rDataBytes.length));
             answers.add(answer);
+            System.out.println("readAnswers " + i + ": " + answer);
         }
         return answers;
     }
@@ -142,6 +143,7 @@ public final class DnsUtil {
             byteBuffer.putShort(answer.getRdLength());
             byte[] rDataBytes = answer.getRData();
             byteBuffer.put(Arrays.copyOf(rDataBytes, rDataBytes.length));
+            System.out.println("writeAnswers: " + answer);
         }
     }
 }
